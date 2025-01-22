1b1ada3bcdbe1f9b0165c2717e7d7179ac7647b9
ray <me@rayhanadev.com>
2025-01-22 01:14:33 -0500

    feat: tag both original author and message author in gh issue

--- src/events/message_create.ts
+++ src/events/message_create.ts
@@ -13,7 +13,11 @@ export async function evergreenIssueWorkflow(message: Message) {
   if (message.author.bot) return;
   if (message.channel.isDMBased()) return;
 
-  if (!message.member?.roles.cache.some((r) => r.id === ORGANIZER_ROLE_ID || r.id === BISHOP_ROLE_ID)) {
+  if (
+    !message.member?.roles.cache.some(
+      (r) => r.id === ORGANIZER_ROLE_ID || r.id === BISHOP_ROLE_ID,
+    )
+  ) {
     return;
   }
 
@@ -35,24 +39,24 @@ export async function evergreenIssueWorkflow(message: Message) {
   if (!original) return;
 
   const people = await getAssociationsFile();
-  const assignees: string | null = people[original.author.id] || null;
+  const assignees: string[] = [
+    people[message.author.id],
+    people[original.author.id],
+  ].filter(Boolean);
 
-  const title = `Evergreen request from @${message.author.tag} in #${message.channel.name}`;
-  const body = `**@${assignees ?? original.author.tag}**[^1] said in **[#${message.channel.name}](<${message.url}>)**:
+  // TODO(rayhanadev): generate title using groq
+  const title = `Evergreen request from @${people[message.author.id] ?? message.author.tag} in #${message.channel.name}`;
+  const body = `**@${people[original.author.id] ?? original.author.tag}**[^1] said in **[#${message.channel.name}](<${message.url}>)**:
 
 ${original.content
   .split("\n")
   .map((line) => `> ${line}`)
   .join("\n")}
 
-[^1]: @${assignees ?? original.author.tag} please edit this issue to include any additional context or details you think are
+[^1]: @${people[message.author.id] ?? message.author.tag} please edit this issue to include any additional context or details you think are
       necessary, and/or assign it to someone else if you would not want to do it.`;
 
-  const { html_url } = await createGithubIssue(
-    title,
-    body,
-    assignees ? [assignees] : [],
-  );
+  const { html_url } = await createGithubIssue(title, body, assignees);
 
   await message.reply(`Created issue: ${html_url}`);
 }
