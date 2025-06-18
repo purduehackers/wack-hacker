5e16a753b856bfd815b84c19640723094f8b0b4e
Rayhan Noufal Arayilakath <me@rayhanadev.com>
2025-06-18 19:54:16 +0000

    feat: filter messages

--- src/events/message_create/dashboard.ts
+++ src/events/message_create/dashboard.ts
@@ -1,20 +1,26 @@
 import { type Message } from "discord.js";
 import { connectToApi, sendDashboardMessage } from "../../utils/phack";
-
-const client = await connectToApi();
+import { INTERNAL_CATEGORIES } from "../../utils/consts";
 
 export default async function handler(message: Message) {
-  if (message.author.bot) return;
-  if (message.channel.isDMBased()) return;
+    if (message.author.bot) return;
+    if (message.channel.isDMBased()) return;
+    if (
+        message.channel.parentId &&
+        INTERNAL_CATEGORIES.includes(message.channel.parentId)
+    )
+        return;
 
-  await sendDashboardMessage(client, {
-    image: message.author.avatarURL(),
-    timestamp: message.createdAt.toISOString(),
-    username: message.author.username,
-    content: message.content,
-    attachments:
-      message.attachments.size > 0
-        ? [...message.attachments.entries().map(([, { url }]) => url)]
-        : undefined,
-  });
+    const client = await connectToApi();
+
+    await sendDashboardMessage(client, {
+        image: message.author.avatarURL(),
+        timestamp: message.createdAt.toISOString(),
+        username: message.author.username,
+        content: message.content,
+        attachments:
+            message.attachments.size > 0
+                ? [...message.attachments.entries().map(([, { url }]) => url)]
+                : undefined,
+    }).finally(() => client.close());
 }
