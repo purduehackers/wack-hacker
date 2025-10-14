8059e2bf843e4b5f6d38e4505608e238bf664071
CynthiaClementine <39636090+CynthiaClementine@users.noreply.github.com>
2025-10-14 12:48:16 -0400

    Added ability to write to mediawiki

--- src/events/message_create/evergreen-it.ts
+++ src/events/message_create/evergreen-it.ts
@@ -1,32 +1,26 @@
 import type { Message } from "discord.js";
 
-import {
-	BISHOP_ROLE_ID,
-	EVERGREEN_CREATE_ISSUE_STRING,
-	ORGANIZER_ROLE_ID,
-} from "../../utils/consts";
+import { BISHOP_ROLE_ID, EVERGREEN_CREATE_ISSUE_STRING, ORGANIZER_ROLE_ID } from "../../utils/consts";
 import { createGithubIssue, getAssociationsFile } from "../../utils/github";
+import { appendMediaWikiPage } from "../../utils/mediawiki";
 
 export default async function handler(message: Message) {
 	if (message.author.bot) return;
 	if (message.channel.isDMBased()) return;
 
-	if (
-		!message.member?.roles.cache.some(
-			(r) => r.id === ORGANIZER_ROLE_ID || r.id === BISHOP_ROLE_ID,
-		)
-	) {
+	if (!message.member?.roles.cache.some((r) => r.id === ORGANIZER_ROLE_ID || r.id === BISHOP_ROLE_ID)) {
 		return;
 	}
 
-	if (!message.content.toLowerCase().startsWith(EVERGREEN_CREATE_ISSUE_STRING))
+	if (!message.content.toLowerCase().startsWith(EVERGREEN_CREATE_ISSUE_STRING)) {
 		return;
+	}
 
 	let original: Message;
 
 	if (!message.reference || !message.reference.messageId) {
 		const messages = await message.channel.messages.fetch({ limit: 2 });
-		const [_, ref] = Array.from(messages.values());
+		const [_, ref:Message] = Array.from(messages.values());
 		original = ref;
 	} else {
 		original = await message.channel.messages.fetch(
@@ -36,33 +30,68 @@ export default async function handler(message: Message) {
 
 	if (!original) return;
 
+	const mediaUrl = await handle_mediawiki(original, message);
+	const githubUrl = await handle_github(original, message);
+
+	//hacky but oh well
+	await message.reply(`Created [github issue](${githubUrl}) and [mediawiki issue](${mediaUrl})!`);
+}
+
+async function handle_github(original:Message, message:Message) {
+	//getting the github handles for people
 	const people = await getAssociationsFile();
 	const assignees: string[] = [
 		people[message.author.id],
 		people[original.author.id],
 	].filter(Boolean);
+	const originalAuthor = people[original.author.id] ?? original.author.tag;
+	const requestor = people[message.author.id] ?? message.author.tag;
 
-	// TODO(rayhanadev): generate title using groq
-	let title = `Evergreen request from @${people[message.author.id] ?? message.author.tag} in #${message.channel.name}`;
+	const originalText = original.content;
+	const messageArgs = message.content.slice(EVERGREEN_CREATE_ISSUE_STRING.length);
+	const messageLink = message.url;
+	const channelName = message.channel.name;
 
-	if (message.content.match(/^evergreen it\s?/i)) {
-		title = (
-			message.content.replace(/^evergreen it\s?/i, "") +
-			` - @${people[message.author.id] ?? message.author.tag} in #${message.channel.name}`
-		).substring(0, 255); // Limit 0-255 to accomadate Github's 256 Issue Title Length Limit
-	}
+	const pretitle = (messageArgs.length > 0) ? 
+		`${messageArgs.slice(1)} -` : 
+		`Evergreen request from`;
+	const title = `${pretitle} @${requestor} in #${channelName}`.slice(0, 255); // Limit to accommodate Github's Issue Title Length Limit
 
-	const body = `**@${people[original.author.id] ?? original.author.tag}**[^1] said in **[#${message.channel.name}](<${message.url}>)**:
-
-${original.content
-	.split("\n")
-	.map((line) => `> ${line}`)
-	.join("\n")}
-
-[^1]: @${people[message.author.id] ?? message.author.tag} please edit this issue to include any additional context or details you think are
-      necessary, and/or assign it to someone else if you would not want to do it.`;
+	const body = `**@${originalAuthor}**[^1] said in **[#${channelName}](<${messageLink}>)**:\n\n`
+		+`${originalText
+			.split("\n")
+			.map((line) => `> ${line}`)
+			.join("\n")}\n\n`
+		+`[^1]: @${requestor} please edit this issue to include any additional context or details you think are necessary, `
+		+`and/or assign it to someone else if you would not want to do it.`;
 
 	const { html_url } = await createGithubIssue(title, body, assignees);
+	return html_url;
+}
 
-	await message.reply(`Created issue: ${html_url}`);
+async function handle_mediawiki(original:Message, message:Message) {
+	const pageTarget = `Evergreen It`;
+	const originalText = original.content;
+	const originalAuthor = original.author.tag;
+	const messageArgs = message.content.slice(EVERGREEN_CREATE_ISSUE_STRING.length);
+	const messageLink = message.url;
+	// const requestor = message.author.tag;
+	const channelName = message.channel.name;
+
+	let now = new Date();
+	let months = [`Jan`,`Feb`,`Mar`,`Apr`,`May`,`Jun`,`Jul`,`Aug`,`Sep`,`Oct`,`Nov`,`Dec`];
+
+	const userTitle = (messageArgs.length > 0) ? 
+		messageArgs.slice(1) : 
+		originalText.slice(0, 90);
+	const title = `in #${channelName} - ${userTitle}`;
+
+	const body = `\n\n* [${messageLink} @${originalAuthor} ${now.getDate()} ${months[now.getMonth()]} ${now.getFullYear()}]: ${title}`;
+
+	const { result } = await appendMediaWikiPage(pageTarget, body, "Wack Hacker - added issue");
+	if (result != `Success`) {
+		return;
+	}
+	//hacky but oh well
+	return `https://evergreen.skywiki.org/wiki/${pageTarget}`;
 }
---
+++ src/utils/mediawiki.ts
@@ -0,0 +1,31 @@
+import { EVERGREEN_WIKI_ENDPOINT } from "./consts";
+import fetch from "node-fetch";
+
+async function getCsrfToken() {
+	const res = await fetch(`${EVERGREEN_WIKI_ENDPOINT}?action=query&meta=tokens&type=csrf&format=json`, {
+		method: "GET",
+		headers: { "Content-Type": "application/json" },
+	});
+	const data = await res.json();
+	return data.query.tokens.csrftoken;
+}
+
+export async function appendMediaWikiPage(pageTitle:string, appendText:string, appendMessage:string) {
+	let token = await getCsrfToken();
+	const params = new URLSearchParams({
+		action: "edit",
+		title: pageTitle,
+		appendtext: appendText,
+		summary: appendMessage,
+		token,
+		format: "json"
+	});
+
+	const res = await fetch(EVERGREEN_WIKI_ENDPOINT, {
+		method: "POST",
+		headers: { "Content-Type": "application/x-www-form-urlencoded" },
+		body: params
+	});
+
+	return await res.json();
+}
