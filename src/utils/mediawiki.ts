46c0226a525452d5cdde8a8bcf1b7866dfb7f410
CynthiaClementine <39636090+CynthiaClementine@users.noreply.github.com>
2025-10-17 17:11:05 -0400

    an attempt is made

--- src/utils/mediawiki.ts
+++ src/utils/mediawiki.ts
@@ -1,30 +1,78 @@
-import { EVERGREEN_WIKI_URL, EVERGREEN_WIKI_ENDPOINT } from "./consts";
+import { EVERGREEN_WIKI_URL, EVERGREEN_WIKI_USER, EVERGREEN_WIKI_ENDPOINT } from "./consts";
+import { env } from "../env";
 
-async function getCsrfToken() {
-	const res = await fetch(`${EVERGREEN_WIKI_URL}${EVERGREEN_WIKI_ENDPOINT}?action=query&meta=tokens&type=csrf&format=json`, {
+async function getToken(tokenType:`csrf`|`login`, cookieStr:string = "") {
+	const res = await fetch(`${EVERGREEN_WIKI_URL}${EVERGREEN_WIKI_ENDPOINT}?action=query&meta=tokens&type=${tokenType}&format=json`, {
 		method: "GET",
-		headers: { "Content-Type": "application/json" },
+		headers: { Cookie: cookieStr },
 	});
 	const data = await res.json();
-	return data.query.tokens.csrftoken;
+	console.log(`got token data: `, data);
+	const pathed = (<any>data).query.tokens;
+	return ((tokenType == `csrf`) ? pathed.csrftoken : pathed.logintoken);
 }
 
-export async function appendMediaWikiPage(pageTitle:string, appendText:string, appendMessage:string) {
-	let token = await getCsrfToken();
-	const params = new URLSearchParams({
-		action: "edit",
-		title: pageTitle,
-		appendtext: appendText,
-		summary: appendMessage,
-		token,
-		format: "json"
-	});
+async function login() {
+	console.log(`attempting to log in!`);
+	//initial request for cookies
+	// const init = await fetch(`${EVERGREEN_WIKI_URL}${EVERGREEN_WIKI_ENDPOINT}`);
+	// const initialSet:string[] = (<any>init.headers).raw()['set-cookie'] || [];
+	// const initCookiesStr = initialSet.map(sc => sc.split(";")[0]).filter(Boolean).join("; ");
 
+	//actual login
+	console.log(`getting token...`);
+	const token = await getToken(`login`);
+	const params = new URLSearchParams({
+		action: "login",
+		lgname: EVERGREEN_WIKI_USER,
+		lgpassword: env.MEDIAWIKI_BOT_KEY,
+		lgtoken: token
+	});
 	const res = await fetch(EVERGREEN_WIKI_URL+EVERGREEN_WIKI_ENDPOINT, {
 		method: "POST",
-		headers: { "Content-Type": "application/x-www-form-urlencoded" },
+		headers: { 
+			"Content-Type": "application/x-www-form-urlencoded",
+		},
 		body: params
 	});
+	console.log(`got token!`, res);
+	return res;
+}
 
-	return await res.json();
+export async function appendMediaWikiPage(pageTitle:string, appendText:string, description:string) {
+	//log in, get csrf token, get cookies
+	const res1 = await login();
+	console.log(`logged in!`, res1);
+
+	const cookies = res1.headers.get("set-cookie")?.split(",") ?? [];
+	const cookiesStr = cookies.join("; ");
+	const token = await getToken(`csrf`, cookiesStr);
+	const params = new URLSearchParams({
+		action: `edit`,
+		format: `json`,
+		title: pageTitle,
+		appendtext: appendText,
+		summary: description,
+		token: token
+	});
+
+	//actual writing finally
+	const res2 = await fetch(EVERGREEN_WIKI_URL+EVERGREEN_WIKI_ENDPOINT, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded",
+			Cookie: cookiesStr
+		},
+		body: params
+	});
+	const trueRes = await res2.json();
+
+	// check for captcha
+	if (trueRes?.edit?.captcha) {
+		console.error("Edit requires CAPTCHA:", trueRes.edit.captcha);
+	}
+	console.log(trueRes);
+	console.log(`MW - posted:`);
+
+	return trueRes;
 }
