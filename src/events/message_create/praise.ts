298c815184b76f1ab59169eadb0092193f560989
Eric Park <me@ericswpark.com>
2025-10-17 21:45:15 -0400

    feat: preparation for the robot uprising

--- src/events/message_create/praise.ts
+++ src/events/message_create/praise.ts
@@ -1,8 +1,14 @@
 import { type Message, TextChannel, ChannelType } from "discord.js";
 import { WACKY_ROLE_ID } from "../../utils/consts";
 
-const ENABLE_PATTERN = /wackity\s+hackity\s+praise\s+me/;
-const DISABLE_PATTERN = /wackity\s+hackity\s+go\s+away/;
+const ENABLE_PATTERNS = [
+  /wackity\s+hackity\s+praise\s+me/,
+  /clanke+r+\s+go\s+away/,
+];
+const DISABLE_PATTERNS = [
+  /wackity\s+hackity\s+go\s+away/,
+  /clanke+r+\s+praise\s+me/,
+];
 
 export default async function handler(message: Message) {
 	if (message.author.bot) return;
@@ -15,15 +21,23 @@ export default async function handler(message: Message) {
 		return;
 	if (message.member === null) return;
 
-	try {
-		if (message.content.match(ENABLE_PATTERN)) {
-			await message.member!.roles.add(WACKY_ROLE_ID);
-			await message.react("🥳");
-		} else if (message.content.match(DISABLE_PATTERN)) {
-			await message.member!.roles.remove(WACKY_ROLE_ID);
-			await message.react("🤐");
-		}
-	} catch (e) {
-		console.error(e);
-	}
+  try {
+    for (const enable_pattern of ENABLE_PATTERNS) {
+      if (message.content.match(enable_pattern)) {
+        await message.member!.roles.add(WACKY_ROLE_ID);
+        await message.react("🥳");
+        return;
+      }
+    }
+
+    for (const disable_pattern of DISABLE_PATTERNS) {
+      if (message.content.match(disable_pattern)) {
+        await message.member!.roles.remove(WACKY_ROLE_ID);
+        await message.react("🤐");
+        return;
+      }
+    }
+  } catch (e) {
+    console.error(e);
+  }
 }
