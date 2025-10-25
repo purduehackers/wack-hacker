ae0b524b10111bab4df12b5042444fef58ec431a
Rayhan Noufal Arayilakath <me@rayhanadev.com>
2025-10-24 23:12:56 -0400

    fix: remove websocket connection

--- src/events/message_create/index.ts
+++ src/events/message_create/index.ts
@@ -1,7 +1,7 @@
 import { Events } from "discord.js";
 
 import autoThreadChannels from "./auto-thread-channels";
-import dashboard from "./dashboard";
+// import dashboard from "./dashboard";
 import evergreenIt from "./evergreen-it";
 import grok from "./grok";
 import hackNightImages from "./hack-night-images";
@@ -12,7 +12,7 @@ import praise from "./praise";
 export const eventType = Events.MessageCreate;
 export {
 	autoThreadChannels,
-	dashboard,
+	// dashboard,
 	evergreenIt,
 	voiceMessageTranscription,
 	welcomer,
