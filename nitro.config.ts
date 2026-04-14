91d4890716ea25b2bc4c6d19c85897625d09ac78
Rayhan Noufal Arayilakath <me@rayhanadev.com>
2026-04-14 11:49:33 -0400

    fix: move queue config into nitro

--- nitro.config.ts
+++ nitro.config.ts
@@ -7,6 +7,12 @@ export default defineConfig({
   vercel: {
     functions: {
       maxDuration: 10 * 60,
+      experimentalTriggers: [
+        {
+          type: "queue/v2beta",
+          topic: "tasks",
+        },
+      ],
     },
   },
 });
